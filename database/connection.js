876976c0366f6c2f76e2005d77032f4ca030d3d6
marcosborges1 <borges.marcos1@gmail.com>
2021-02-14 16:22:40 -0300

    Setar porta

--- database/connection.js
+++ database/connection.js
@@ -3,7 +3,8 @@ require("dotenv-safe").config();
 const knex = require("knex")({
     client: "mysql2",
     connection: {
-        host : process.env.host_db,     
+        host : process.env.host_db, 
+        port:3306,    
         user : process.env.user_db, 
         password : process.env.pass_db,
         database : process.env.db 
