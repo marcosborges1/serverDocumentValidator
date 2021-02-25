546c79543e38d992b43d145cdfb7300e9dbe1cae
marcosborges1 <borges.marcos1@gmail.com>
2021-02-25 02:03:32 -0300

    Versão com conexão ok

--- database/connection.js
+++ database/connection.js
@@ -1,32 +1,13 @@
-// require("dotenv-safe").config();
-// // const dbSocketPath = process.env.DB_SOCKET_PATH || '/cloudsql';
-// const knex = require("knex")({
-//     client: "mysql2",
-//     connection: {
-//         host : process.env.host_db, 
-//         // socketPath: `${dbSocketPath}/${process.env.CLOUD_SQL_CONNECTION_NAME}`,
-//         user : process.env.user_db, 
-//         password : process.env.pass_db,
-//         database : process.env.db
-//      }
-// });
-
-// module.exports = knex
-
 require("dotenv-safe").config();
 const dbSocketPath = process.env.DB_SOCKET_PATH || '/cloudsql';
 const knex = require("knex")({
     client: "mysql2",
     connection: {
         // host : process.env.host_db, 
-        host: `${dbSocketPath}/${process.env.CLOUD_SQL_CONNECTION_NAME}`,
+        socketPath: `${dbSocketPath}/${process.env.CLOUD_SQL_CONNECTION_NAME}`,
         user : process.env.user_db, 
         password : process.env.pass_db,
         database : process.env.db 
-     },
-     pool: { 
-        min: 0, 
-        max: 7
-    }
+     }
 });
 module.exports = knex
